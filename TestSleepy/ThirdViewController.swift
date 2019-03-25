62f53185148b7365ad318d82cd2c4dbb37d1a1e1
Kelly Cole <kmcole@Kellys-MacBook-Pro.local>
2019-03-25 19:43:33 -0400

    added calculations for time to rise

---
+++ TestSleepy/ThirdViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ThirdViewController.swift
+//  TestSleepy
+//
+//  Created by Kelly Cole on 3/25/19.
+//  Copyright © 2019 Kelly Cole. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // *** Create date ***
+        let date = NSDate()
+        
+        // *** create calendar object ***
+        var calendar = NSCalendar.current
+        
+        // *** Get components using current Local & Timezone ***
+        print(calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date as Date))
+        
+        // *** define calendar components to use as well Timezone to UTC ***
+        let unitFlags = Set<Calendar.Component>([.hour, .year, .minute])
+        calendar.timeZone = TimeZone(identifier: "UTC")!
+        
+        // *** Get All components from date ***
+        let components = calendar.dateComponents(unitFlags, from: date as Date)
+        print("All Components : \(components)")
+        
+        // *** Get Individual components from date ***
+        let hour = calendar.component(.hour, from: date as Date)
+        let minutes = calendar.component(.minute, from: date as Date)
+        let seconds = calendar.component(.second, from: date as Date)
+
+        Label1.text = "\((hour-4)%24 ):\(minutes)";
+        Label2.text = "\( (hour-4+3)%24 ):\(minutes)";
+        Label3.text = "\( (hour-4+6)%24 ):\(minutes)"
+        Label4.text = "\( (hour-4+9)%24 ):\(minutes)";
+        Label5.text = "\( (hour-4+12)%24 ):\(minutes)";
+        Label6.text = "\( (hour-4+15)%24 ):\(minutes)";
+
+        
+    }
+    
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var Label2: UILabel!
+    @IBOutlet weak var Label3: UILabel!
+    @IBOutlet weak var Label4: UILabel!
+    @IBOutlet weak var Label5: UILabel!
+    @IBOutlet weak var Label6: UILabel!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
