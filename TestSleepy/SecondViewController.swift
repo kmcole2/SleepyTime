b84f0bff52efacef036fca3ad37258d1698f1666
Kelly Cole <kmcole@kellys-mbp.attlocal.net>
2019-03-24 20:58:14 -0400

    Updated view with segmented control, datepicker, multiple view controllers

---
+++ TestSleepy/SecondViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SecondViewController.swift
+//  TestSleepy
+//
+//  Created by Kelly Cole on 3/24/19.
+//  Copyright © 2019 Kelly Cole. All rights reserved.
+//
+
+import UIKit
+
+var selectedTime = 0;
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBAction func indexChanged(_ sender: Any) {
+        print("segmented Control changed")
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            textLabel.text = "First Segment Selected"
+        case 1:
+            textLabel.text = "Second Segment Selected"
+        default:
+            break
+        }
+    }
+
+    @IBAction func Submit(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+    
+        var strDate = dateFormatter.string(from: timePicker.date)
+        textLabel.text = strDate
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TestSleepy/ViewController.swift
+++ TestSleepy/ViewController.swift
@@ -14,11 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-//    @IBAction func GoingNowClick(_ sender: Any) {
-
-//    }
-    
     @IBAction func GoingNowClicked(_ sender: Any) {
         print("TestSegue")
         performSegue(withIdentifier: "TestSegue", sender: sender);
@@ -28,25 +23,5 @@ class ViewController: UIViewController {
         print("PickTimeClick")
         performSegue(withIdentifier: "PickTimeClick", sender: sender);
     }
-    
-
-    //    @IBAction func GoingNow(_ sender: Any) {
-//        println("Pan segue from mainVC to Life Lists did run")
-//
-//    }
-    
-//    override func performSegue(withIdentifier identifier: String, sender: Any?) {
-//        print("segue - \GoingNowClick")
-//
-//        if let destinationViewController = segue.destination as? GoingNowClick {
-//            if let button = sender as? UIButton {
-//                secondViewController.<buttonIndex> = button.tag
-//                // Note: add/define var buttonIndex: Int = 0 in <YourDestinationViewController> and print there in viewDidLoad.
-//            }
-//            
-//        }
-//    }
-
-
 }
 
